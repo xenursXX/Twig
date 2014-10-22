e5ed6229fe7c30996ba33c7b0a940730ba932042
guillaume <gui.nouhaud@gmail.com>
2014-10-22 13:01:00 +0200

    remove slugify

--- index.php
+++ index.php
@@ -3,4 +3,22 @@
 require __DIR__ .'/vendor/autoload.php';
 
 
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem([
+    __DIR__.'/views',
+]);
 
+$twig = new Twig_Environment($loader, [
+    //'cache => null,
+]);
+
+
+$article = [
+    'name' => 'fhuyeizU',
+        'content' => 'rdug drtd',
+];
+
+echo $twig->render('article.html.twig', [
+    'article' => $article,
+
+    ]);
